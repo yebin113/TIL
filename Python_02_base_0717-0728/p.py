bdfc219e02676eadab1f814b45bd89d8c72fde91
ybyb99 <yebin4684@gmail.com>
2023-07-31 17:23:32 +0900

    0731

---
+++ Python_02_base_0717-0728/p.py
@@ -0,0 +1,15 @@
+# my_name = "        apple         "
+
+# print(my_name.find('a'))
+
+# print(my_name.replace('a','pine'))
+# print(my_name.split('p'))
+# print(my_name.strip())
+
+# my_list = ['a','p','p','l','e']
+# print(''.join(my_list))
+
+numbers = [3,2,1,5,32,1]
+
+numbers.sort(reverse=0)
+print(numbers)
